10105389ca9e5a893134a328fe1020a3e9fd87c0
Vishwajeet Singh Jii <vishwajeetsinghbinay@gmail.com>
2026-03-29 23:27:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+class Solution {
+    public static void fizzBuzz(int number) {
+        if(number%3==0 && number%5==0){
+            System.out.println("FizzBuzz");
+        }else if(number%3==0){
+            System.out.println("Fizz");
+        }else if(number%5==0){
+            System.out.println("Buzz");
+        }else{
+            System.out.println(number);
+        }
+    }   
+}
